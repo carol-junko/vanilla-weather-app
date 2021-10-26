94029fcb6bba84e02d8ef27b6959e2aeb8449e7a
carol-junko <70997293+carol-junko@users.noreply.github.com>
2021-10-26 22:17:32 +1100

    Weather API Installed

---
+++ src/app.js
@@ -0,0 +1,38 @@
+function selectElement(element){
+    return document.querySelector(element);
+}
+
+function getWeatherInfo(response){
+    console.log(response.data);
+    let cityElement = selectElement("#city");
+    cityElement.innerHTML = response.data.name;
+
+    let weatherDescriptionElement = selectElement("#weather-description");
+    weatherDescriptionElement.innerHTML = response.data.weather[0].description;
+
+    let temperatureElement = selectElement("#temperature");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+    let humidityElement = selectElement("#humidity");
+    humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+
+    let windElement = selectElement("#wind");
+    windElement.innerHTML = `Wind: ${Math.round((response.data.wind.speed)*3.6)} km/hr`
+
+    let iconImage = selectElement("#icon-description");
+    iconImage.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconImage.setAttribute("alt", response.data.weather[0].description);
+}
+
+
+
+let city = "Melbourne";
+let apiKey = "de31afd036b8bd58c684a452ffef335f";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(getWeatherInfo);
+
+
+
+
+
